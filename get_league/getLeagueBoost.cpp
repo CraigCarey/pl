20abf980caf182a5cea934670cb0dc4b77552190
Craig <sgccarey@gmail.com>
2017-05-26 13:09:08 +0100

    Modified boost sync_client example, prints json to cmdline

--- get_league/getLeagueBoost.cpp
+++ get_league/getLeagueBoost.cpp
@@ -1,31 +1,99 @@
-#include <boost/asio.hpp>
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <boost/asio.hpp>
 
-int main()
+using boost::asio::ip::tcp;
+
+const std::string hostUrl = "www.footballwebpages.co.uk";
+const std::string requestLeague = "/league.json?comp=1";
+
+int main(int argc, char* argv[])
 {
-	boost::system::error_code ec;
-	using namespace boost::asio;
+	try
+	{
+		boost::asio::io_service io_service;
 
-	io_service svc;
-	ip::tcp::socket sock(svc);
-	sock.connect({ {}, 8087 });
+		// Get a list of endpoints corresponding to the server name
+		tcp::resolver resolver(io_service);
+		tcp::resolver::query query("www.footballwebpages.co.uk", "http");
+		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 
-	// send request
-	std::string request("GET /newGame?name=david HTTP/1.1\r\n\r\n");
-	sock.send(buffer(request));
+		// Try each endpoint until we successfully establish a connection
+		tcp::socket socket(io_service);
+		boost::asio::connect(socket, endpoint_iterator);
 
-	// read response
-	std::string response;
+		// Form the request. We specify the "Connection: close" header so that the
+		// server will close the socket after transmitting the response. This will
+		// allow us to treat all data up until the EOF as the content
+		boost::asio::streambuf request;
+		std::ostream request_stream(&request);
+		request_stream << "GET " << "/league.json?comp=1" << " HTTP/1.0\r\n";
+		request_stream << "Host: " << "www.footballwebpages.co.uk" << "\r\n";
+		request_stream << "Accept: */*\r\n";
+		request_stream << "Connection: close\r\n\r\n";
 
-	do {
+		// Send the request.
+		boost::asio::write(socket, request);
 
-		char buf[1024];
-		size_t bytes_transferred = sock.receive(buffer(buf), {}, ec);
-		if (!ec) response.append(buf, buf + bytes_transferred);
+		// Read the response status line. The response streambuf will automatically
+		// grow to accommodate the entire line. The growth may be limited by passing
+		// a maximum size to the streambuf constructor.
+		boost::asio::streambuf response;
+		boost::asio::read_until(socket, response, "\r\n");
 
+		// Check that response is OK
+		std::istream response_stream(&response);
+		std::string http_version;
+		response_stream >> http_version;
+		unsigned int status_code;
+		response_stream >> status_code;
+		std::string status_message;
+		std::getline(response_stream, status_message);
+
+		if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+		{
+			std::cout << "Invalid response\n";
+			return 1;
+		}
+
+		if (status_code != 200)
+		{
+			std::cout << "Response returned with status code " << status_code << "\n";
+			return 1;
+		}
+
+		// Read the response headers, which are terminated by a blank line
+		boost::asio::read_until(socket, response, "\r\n\r\n");
+
+		// Process the response headers
+		std::string header;
+		while (std::getline(response_stream, header) && header != "\r")
+		std::cout << header << "\n";
+		std::cout << "\n";
+
+		// Write whatever content we already have to output
+		if (response.size() > 0)
+		std::cout << &response;
+
+		// Read until EOF, writing data to output as we go
+		boost::system::error_code error;
+
+		while (boost::asio::read(socket, response, boost::asio::transfer_at_least(1), error))
+		{
+			std::cout << &response;
+		}
+
+		if (error != boost::asio::error::eof)
+		{
+			throw boost::system::system_error(error);
+		}
 	}
-	while (!ec);
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << "\n";
+	}
 
-	// print and exit
-	std::cout << "Response received: '" << response << "'\n";
+	return 0;
 }
---
+++ get_league/getLeagueHttp.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string>
+
+#define HTTP_PORT 80
+
+int main()
+{
+	std::string server = "www.footballwebpages.co.uk/feeds";
+
+}
