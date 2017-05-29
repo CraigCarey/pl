184bb4fc38a0ceb554c3026e20f2c33af9aaca3d
Craig <sgccarey@gmail.com>
2017-05-29 23:41:04 +0100

    jsonParser parses nested array of teams

--- get_league/jsonParser.cpp
+++ get_league/jsonParser.cpp
@@ -1,17 +1,18 @@
+// TODO: Make league, team proper classes
+//use C++11 for loop instead of BOOST_FOREACH
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
 #include <string>
-#include <set>
+#include <vector>
 #include <exception>
 #include <iostream>
+
 namespace pt = boost::property_tree;
 
 struct team
 {
 	int position;
-	char outcome;
 	std::string name;
 	int played;
 	int won;
@@ -27,7 +28,7 @@ struct league
 {
 	std::string competition;
 	std::string description;
-	std::set<std::string> teams;
+	std::vector<team> teams;
 	void load(const std::string &filename);
 	void save(const std::string &filename);
 };
@@ -40,19 +41,33 @@ void league::load(const std::string &filename)
 	// Parse the JSON into the property tree
 	pt::read_json(filename, tree);
 
-	// Use the throwing version of get to find the competition description
+	// Use the throwing version of get to find the competition name & description
 	// If the path cannot be resolved, an exception is thrown
 	competition = tree.get<std::string>("leagueTable.competition");
 	description = tree.get<std::string>("leagueTable.description");
 
-	// Use get_child to find the node containing the modules, and iterate over
-	// its children. If the path cannot be resolved, get_child throws.
-	// A C++11 for-range loop would also work.
+	// Use get_child to find the node containing the teams, and iterate over
+	// its children. If the path cannot be resolved, get_child throws
+	// TODO: C++11 for-range loop would also work
 	BOOST_FOREACH(pt::ptree::value_type &v, tree.get_child("leagueTable.team"))
 	{
-		std::cout << v.second.data() << std::endl;
-		// The data function is used to access the data stored in a node.
-		teams.insert(v.second.data());
+		std::string name;
+		pt::ptree subTree;
+		std::tie(name, subTree) = v;
+
+		team tm;
+		tm.position = subTree.get<int>("position");
+		tm.name = subTree.get<std::string>("name");
+		tm.played = subTree.get<int>("played");
+		tm.won = subTree.get<int>("won");
+		tm.drawn = subTree.get<int>("drawn");
+		tm.lost = subTree.get<int>("lost");
+		tm.goalsFor = subTree.get<int>("for");
+		tm.goalsAgainst = subTree.get<int>("against");
+		tm.goalDifference = subTree.get<int>("goalDifference");
+		tm.points = subTree.get<int>("points");
+
+		teams.push_back(tm);
 	}
 }
 
@@ -61,12 +76,16 @@ int main()
 	try
 	{
 		league lg;
-		lg.load("pl1.json");
-		std::cout << "Success\n";
+		lg.load("pl.json");
+
+		for (team t : lg.teams)
+		{
+			std::cout << t.name << std::endl;
+		}
 	}
 	catch (std::exception &e)
 	{
-		std::cout << "Error: " << e.what() << "\n";
+		std::cerr << "Error: " << e.what() << "\n";
 	}
 
 	return 0;
